a628538e6c21961bc5d5a56e773bd44d5993bbe5
connectedreasoning <manuelhe46@gmail.com>
2016-09-16 15:46:48 -0700

    fiddle with output

--- face.py
+++ face.py
@@ -2,17 +2,13 @@ from graphics import *
 
 
 def main():
-    win = GraphWin('Face', 800, 450) # give title and dimensions
+    win = GraphWin('Face', 800, 650) # give title and dimensions
     #win.yUp() # make right side up coordinates!
     for x in range(0,600,5):
-        pt = Point(x,x)
+        pt = Point(600-x,x)
         pt.setFill("red")
         pt.draw(win)
 
-    for x in range(600,0,-5):
-        pt = Point(600+x,x)
-        pt.setFill("blue")
-        pt.draw(win)
 
     win.getMouse()
     win.close()
