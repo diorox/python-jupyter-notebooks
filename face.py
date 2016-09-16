bbab077b03c1f714c266f61642d8bc999c5e1f18
Hernandez.Manuel <HernandezM@ace.aaaclubnet.com>
2016-09-16 14:41:27 -0700

    added face

--- face.py
+++ face.py
@@ -2,30 +2,18 @@ from graphics import *
 
 
 def main():
-    win = GraphWin('Face', 200, 150) # give title and dimensions
+    win = GraphWin('Face', 800, 450) # give title and dimensions
     #win.yUp() # make right side up coordinates!
+    for x in range(0,600,5):
+        pt = Point(x,x)
+        pt.setFill("red")
+        pt.draw(win)
 
-    head = Circle(Point(40,100), 25) # set center and radius
-    head.setFill("yellow")
-    head.draw(win)
+    for x in range(600,0,-5):
+        pt = Point(600+x,x)
+        pt.setFill("blue")
+        pt.draw(win)
 
-    eye1 = Circle(Point(30, 105), 5)
-    eye1.setFill('blue')
-    eye1.draw(win)
-
-    eye2 = Line(Point(45, 105), Point(55, 105)) # set endpoints
-    eye2.setWidth(3)
-    eye2.draw(win)
-
-    mouth = Oval(Point(30, 90), Point(50, 85)) # set corners of bounding box
-    mouth.setFill("red")
-    mouth.draw(win)
-
-    label = Text(Point(100, 120), 'A face')
-    label.draw(win)
-
-    message = Text(Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
-    message.draw(win)
     win.getMouse()
     win.close()
 
